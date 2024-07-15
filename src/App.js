d80649d11be7c5ad4090e3a7daaf4a9aa4fd0eb8
Neway Aragaw <niwaygebrie.com>
2024-07-14 20:48:23 -0400

    first committ

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Training from './component/Training'; // Assuming Training is in 'components' folder
 import TrainingHome from './component/TrainingHome';
+import WorkingEnvironment from './component/Working-environment';
+import MultipleChoiceTest from './component/MultipleChoice';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Training />} />
         <Route path="/training-home" element={<TrainingHome />} />
+        <Route path="/working-environment" element={<WorkingEnvironment />} />
+        <Route path="/multiple-choice" element={<MultipleChoiceTest />} />
       </Routes>
  
   );
---
+++ src/component/Working-environment.js
@@ -0,0 +1,40 @@
+import React, { useState, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
+import "./Working-env.css"
+import workingVideo from "../asset/videoPSA.mp4"
+
+function WorkingEnvironment() {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const videoRef = useRef(null);
+  const navigate = useNavigate();
+
+  const handlePlayPause = () => {
+    if (isPlaying) {
+      videoRef.current.pause();
+    } else {
+      videoRef.current.play();
+    }
+    setIsPlaying(!isPlaying);
+  };
+
+  const handleVideoEnd = () => {
+    navigate('/multiple-choice'); // Navigate to the multiple choice page
+  };
+
+  return (
+    <div className="video-player">
+        <h1>video 1-Understanding the work Environment</h1>
+      <video
+        ref={videoRef}
+        src={workingVideo}// Replace with the actual path to your video
+        controls
+        onEnded={handleVideoEnd}
+      />
+      <button onClick={handlePlayPause}>
+        {isPlaying ? 'Pause' : 'Play'}
+      </button>
+    </div>
+  );
+}
+
+export default WorkingEnvironment;
